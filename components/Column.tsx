3383e2a8deda5eba2fc9cab1d8660f45e5c595c4
SyntaxLexx <spencermainaa@gmail.com>
2023-05-31 04:44:34 +0300

    feat: can search via todos

--- components/Column.tsx
+++ components/Column.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Draggable, Droppable } from "react-beautiful-dnd";
 import TodoCard from "./TodoCard";
 import { PlusCircleIcon } from "@heroicons/react/24/solid";
+import { useBoardStore } from "@/store/BoardStore";
 
 type Props = {
   id: TypedColumn;
@@ -19,6 +20,8 @@ const idToColumnText: {
 };
 
 function Column({ id, index, todos }: Props) {
+  const [searchString] = useBoardStore((state) => [state.searchString]);
+
   return (
     <Draggable draggableId={id} index={index}>
       {(provided) => (
@@ -39,29 +42,45 @@ function Column({ id, index, todos }: Props) {
                 <h2 className="flex justify-between font-bold text-xl ">
                   {idToColumnText[id]}
                   <span className="text-gray-500 bg-gray-200 rounded-full px-2 py-1 text-sm font-normal">
-                    {todos.length}
+                    {!searchString
+                      ? todos.length
+                      : todos.filter((todo) =>
+                          todo.title
+                            .toLowerCase()
+                            .includes(searchString.toLowerCase())
+                        ).length}
                   </span>
                 </h2>
 
                 <div className="space-y-2">
-                  {todos.map((todo, index) => (
-                    <Draggable
-                      key={todo.$id}
-                      draggableId={todo.$id}
-                      index={index}
-                    >
-                      {(provided) => (
-                        <TodoCard
-                          todo={todo}
-                          index={index}
-                          id={id}
-                          innerRef={provided.innerRef}
-                          draggableProps={provided.draggableProps}
-                          dragHandleProps={provided.dragHandleProps}
-                        ></TodoCard>
-                      )}
-                    </Draggable>
-                  ))}
+                  {todos.map((todo, index) => {
+                    if (
+                      searchString &&
+                      !todo.title
+                        .toLowerCase()
+                        .includes(searchString.toLowerCase())
+                    )
+                      return null;
+
+                    return (
+                      <Draggable
+                        key={todo.$id}
+                        draggableId={todo.$id}
+                        index={index}
+                      >
+                        {(provided) => (
+                          <TodoCard
+                            todo={todo}
+                            index={index}
+                            id={id}
+                            innerRef={provided.innerRef}
+                            draggableProps={provided.draggableProps}
+                            dragHandleProps={provided.dragHandleProps}
+                          ></TodoCard>
+                        )}
+                      </Draggable>
+                    );
+                  })}
 
                   {provided.placeholder}
 
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,22 +3,28 @@
 import Image from "next/image";
 import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 import Avatar from "react-avatar";
+import { useBoardStore } from "@/store/BoardStore";
 
 function Header() {
+  const [searchString, setSearchString] = useBoardStore((state) => [
+    state.searchString,
+    state.setSearchString,
+  ]);
+
   return (
     <header>
       <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
         <div
           className="
-          absolute 
-          top-0 
-          left-0 
-          w-full 
-          h-96 
-          bg-gradient-to-br 
+            absolute 
+            top-0 
+            left-0 
+            w-full 
+            h-96 
+            bg-gradient-to-br 
           from-pink-400 
           to-primary 
-          rounded-md 
+            rounded-md 
             filter 
             blur-3xl 
             opacity-50 
@@ -44,6 +50,8 @@ function Header() {
               type="text"
               placeholder="Search"
               className="flex-1 outline-none p-2"
+              value={searchString}
+              onChange={(e) => setSearchString(e.target.value)}
             ></input>
             <button hidden type="submit">
               Search
